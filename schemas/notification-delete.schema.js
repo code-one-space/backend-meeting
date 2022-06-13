7d197e804c70570db2782be9afbfa451767fbb67
nojomyth-dev <nojomyth@gmx.de>
2022-06-13 16:30:18 +0200

    fixed required field

--- schemas/notification-delete.schema.js
+++ schemas/notification-delete.schema.js
@@ -3,7 +3,7 @@ const Joi = require("joi-oid")
 const schema = Joi.object({
     meetingId: Joi.objectId().required(),
     receiverId: Joi.objectId().required(),
-    notificationId: Joi.objectId(),
+    notificationId: Joi.objectId().required(),
 })
 
 module.exports.notificationDeleteSchema = schema
